c666d962cd756d286cb9546c6ff3e5e7492db9e7
Reagan Mbitiru <reagan.mbitiru@ucglobalprograms.org>
2019-02-11 17:58:47 +0300

    cleared textarea on deselection

--- DreamsApp/static/js/site.js
+++ DreamsApp/static/js/site.js
@@ -3278,6 +3278,7 @@ $(document).ready(function () {
             $('label#reason_for_exit_error').hide();
         } else if (selectedOption == LOST_TO_FOLLOW_UP_CODE) {
             $('div#reason_for_exit_other_section').addClass('hidden');
+            $('div#reason_for_exit_other_section textarea#reason_for_exit_other').val('');
             if (followupAttempts < MIN_UNSUCCESSFUL_FOLLOW_UP_ATTEMPTS) {
                 console.log('error');
                 $('label#reason_for_exit_error').text('Warning: client has less than 4 follow up attempts');
