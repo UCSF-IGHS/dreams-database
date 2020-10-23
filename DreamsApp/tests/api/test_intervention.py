752f3682e744b543c60bb304cc3d3300cf1ed3cd
aonomike <aonomike@gmail.com>
2020-10-23 06:49:34 +0300

    Extract set up intervention function

--- DreamsApp/tests/api/test_intervention.py
+++ DreamsApp/tests/api/test_intervention.py
@@ -44,6 +44,9 @@ class InterventionAPITestCase(TestCase):
             },
         ]
 
+    def tearDown(self):
+        del self.interventions
+
     def test_with_no_authentication_is_unauthorised(self):
         factory = APIRequestFactory()
         request = factory.post("api/v1/interventions")
@@ -307,24 +310,8 @@ class InterventionAPITestCase(TestCase):
         assert response.data["message"] == [{"client": ["This field is required."]}]
 
     def test_authenticated_request_with_null_client_id_supplied_returns_400(self):
-
-        intervention_category = InterventionCategoryFactory()
-        intervention_type = InterventionTypeFactory(
-            intervention_category_id=intervention_category.id
-        )
         user = User.objects.create(username="adventure", password="No1Knows!t")
-        external_organisation_type = ExternalOrganisationTypeFactory()
-        external_organisation = ExternalOrganisationFactory(
-            type_id=external_organisation_type.id
-        )
-        pregnancy_test_result = PregnancyTestResultFactory()
-        implementing_partner = ImplementingPartnerFactory()
-        self.interventions[0]["intervention_type"] = intervention_type.code
-        self.interventions[0]["hts_result"] = None
-        self.interventions[0]["external_organisation"] = external_organisation.code
-        self.interventions[0]["pregnancy_test_result"] = pregnancy_test_result.code
-        self.interventions[0]["created_by"] = user.username
-        self.interventions[0]["implementing_partner"] = implementing_partner.code
+        self._setup_intervention(user)
         self.interventions[0]["client"] = None
         factory = APIRequestFactory()
         request = factory.post(
@@ -336,3 +323,25 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data["message"] == [{"client": ["This field may not be null."]}]
+
+    def _setup_intervention(self, user):
+
+        client = Client.objects.create(first_name="Lady", last_name="Bird")
+        intervention_category = InterventionCategoryFactory()
+        intervention_type = InterventionTypeFactory(
+            intervention_category_id=intervention_category.id
+        )
+        external_organisation_type = ExternalOrganisationTypeFactory()
+        external_organisation = ExternalOrganisationFactory(
+            type_id=external_organisation_type.id
+        )
+        pregnancy_test_result = PregnancyTestResultFactory()
+        implementing_partner = ImplementingPartnerFactory()
+        self.interventions[0]["client"] = client.id
+        self.interventions[0]["intervention_type"] = intervention_type.code
+        self.interventions[0]["hts_result"] = None
+        self.interventions[0]["external_organisation"] = external_organisation.code
+        self.interventions[0]["pregnancy_test_result"] = pregnancy_test_result.code
+        self.interventions[0]["created_by"] = user.username
+        self.interventions[0]["implementing_partner"] = implementing_partner.code
+        self.interventions[0]["client"] = None
