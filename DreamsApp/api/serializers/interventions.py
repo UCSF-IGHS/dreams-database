9302409076133b00d4661addf1644dc897cd3ac4
aonomike <aonomike@gmail.com>
2020-11-02 08:42:17 +0300

    Refactor Tests
    - Use SlugRelatedField on serializer to make code cleaner
    - Reformat code with pycharm

--- DreamsApp/api/serializers/interventions.py
+++ DreamsApp/api/serializers/interventions.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from DreamsApp.models import Intervention
+from DreamsApp.models import Intervention, HTSResult, InterventionType, PregnancyTestResult, ImplementingPartner, \
+    ExternalOrganisation, Client
 
 
 class InterventionSerializer(serializers.ModelSerializer):
@@ -16,6 +18,14 @@ class InterventionSerializer(serializers.ModelSerializer):
 
 
 class InterventionListSerializer(serializers.ModelSerializer):
+    hts_result = serializers.SlugRelatedField(many=False, queryset=HTSResult.objects.all(), slug_field='code', read_only=False, allow_null=True)
+    pregnancy_test_result = serializers.SlugRelatedField(many=False, queryset=PregnancyTestResult.objects.all(), slug_field='code', read_only=False, allow_null=True)
+    intervention_type = serializers.SlugRelatedField(many=False, queryset=InterventionType.objects.all(), slug_field='code', read_only=False)
+    implementing_partner = serializers.SlugRelatedField(many=False, queryset=ImplementingPartner.objects.all(), slug_field='code', read_only=False)
+    external_organisation = serializers.SlugRelatedField(many=False, queryset=ExternalOrganisation.objects.all(), slug_field='code', read_only=False, allow_null=True)
+    client = serializers.SlugRelatedField(many=False, queryset=Client.objects.all(), slug_field='id', read_only=False)
+    created_by = serializers.SlugRelatedField(many=False, queryset=User.objects.all(), slug_field='username', read_only=False)
+
     class Meta:
         model = Intervention
         fields = (
@@ -28,7 +38,8 @@ class InterventionListSerializer(serializers.ModelSerializer):
             "created_by",
             "hts_result",
             "client",
+            "external_organisation",
             "pregnancy_test_result",
             "intervention_type",
-            "implementing_partner",
+            "implementing_partner"
         )
--- DreamsApp/tests/api/test_intervention.py
+++ DreamsApp/tests/api/test_intervention.py
@@ -1,13 +1,7 @@
-import json
-from rest_framework import status
 from django.test import TestCase, Client
 from rest_framework.test import APIRequestFactory, force_authenticate
 from django.contrib.auth.models import User
 from datetime import date, timedelta
-from DreamsApp.api.serializers.interventions import (
-    InterventionSerializer,
-    InterventionListSerializer,
-)
 from DreamsApp.api.views.interventions import InterventionMultipleCreateView
 from DreamsApp.tests.factories.factories import (
     ExternalOrganisationFactory,
@@ -23,7 +17,6 @@ from DreamsApp.models import Client, ExternalOrganisation, Intervention
 
 class InterventionAPITestCase(TestCase):
     def setUp(self):
-
         self.interventions = [
             {
                 "intervention_date": date.today(),
@@ -67,7 +60,7 @@ class InterventionAPITestCase(TestCase):
         assert response.data["message"] == "The request body was empty"
 
     def test_authenticated_request_with_empty_array_body_responds_with_bad_request(
-        self,
+            self,
     ):
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self.interventions = []
@@ -85,11 +78,10 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data[
-            "message"
-        ] == "The supplied client {} does not exist".format(WRONG_CLIENT_ID)
+                   "message"
+               ] == [{'client': ['Object with id={} does not exist.'.format(WRONG_CLIENT_ID)]}]
 
     def test_authenticated_request_with_wrong_intervention_type_returns_400(self):
-
         WRONG_INTERVENTION_TYPE = 99999
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self._setup_intervention(user)
@@ -98,13 +90,10 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data[
-            "message"
-        ] == "The supplied InterventionType {} does not exist".format(
-            WRONG_INTERVENTION_TYPE
-        )
+                   "message"
+               ] == [{'intervention_type': ['Object with code={} does not exist.'.format(WRONG_INTERVENTION_TYPE)]}]
 
     def test_authenticated_request_with_wrong_external_organization_returns_400(self):
-
         WRONG_EXTERNAL_ORGANISATION = 9999
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self._setup_intervention(user)
@@ -113,10 +102,9 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data[
-            "message"
-        ] == "The supplied ExternalOrganization {} does not exist".format(
-            WRONG_EXTERNAL_ORGANISATION
-        )
+                   "message"
+               ] == [{'external_organisation': [
+            'Object with code={} does not exist.'.format(WRONG_EXTERNAL_ORGANISATION)]}]
 
     def test_authenticated_request_with_wrong_hts_result_returns_400(self):
         WRONG_HTS_RESULT = 9999
@@ -127,10 +115,10 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data[
-            "message"
-        ] == "The supplied HTSResult {} does not exist".format(WRONG_HTS_RESULT)
+                   "message"
+               ] == [{'hts_result': ['Object with code={} does not exist.'.format(WRONG_HTS_RESULT)]}]
 
-    def test_authenticated_request_with_missing_pregnancy_test_returns_400(self):
+    def test_authenticated_request_with_wrong_pregnancy_test_returns_400(self):
         MISSING_PREGNANCY_TEST = 9999
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self._setup_intervention(user)
@@ -139,13 +127,11 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data[
-            "message"
-        ] == "The supplied PregnancyTestResult {} does not exist".format(
-            MISSING_PREGNANCY_TEST
-        )
+                   "message"
+               ] == [{'pregnancy_test_result': ['Object with code={} does not exist.'.format(MISSING_PREGNANCY_TEST)]}]
 
     def test_authenticated_request_with_wrong_implementing_partner_test_returns_404(
-        self,
+            self,
     ):
         IMPLENTING_PARTNER_NOT_IN_DATABASE = 9999
         user = User.objects.create(username="adventure", password="No1Knows!t")
@@ -157,22 +143,22 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data[
-            "message"
-        ] == "The supplied ImplementingPartner {} does not exist".format(
-            IMPLENTING_PARTNER_NOT_IN_DATABASE
-        )
+                   "message"
+               ] == [{'implementing_partner': [
+            'Object with code={} does not exist.'.format(IMPLENTING_PARTNER_NOT_IN_DATABASE)]}]
 
-    def test_authenticated_request_with_wrong_user_returns_404(self):
-
+    def test_authenticated_request_with_wrong_user_returns_400(self):
+        WRONG_USERNAME = "user_who_did_not_create_the_intervention"
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self._setup_intervention(user)
         self.interventions[0]["created_by"] = "user_who_did_not_create_the_intervention"
         response = self._send_request(user)
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
+        assert response.data["message"] == [
+            {'created_by': ['Object with username={} does not exist.'.format(WRONG_USERNAME)]}]
 
     def test_authenticated_request_with_valid_request_data_creates_record(self):
-
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self._setup_intervention(user)
         self.interventions[0]["hts_result"] = None
@@ -182,7 +168,6 @@ class InterventionAPITestCase(TestCase):
         assert response.data["message"] == "Success! Records successfully created"
 
     def test_authenticated_request_with_no_client_id_supplied_returns_400(self):
-
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self._setup_intervention(user)
         del self.interventions[0]["client"]
@@ -190,6 +175,16 @@ class InterventionAPITestCase(TestCase):
         assert response.status_code == 400
         assert response.status_text == "Bad Request"
         assert response.data["message"] == [{"client": ["This field is required."]}]
+
+    def test_authenticated_request_with_wrong_client_id_supplied_returns_400(self):
+        WRONG_CLIENT_ID = 99999
+        user = User.objects.create(username="adventure", password="No1Knows!t")
+        self._setup_intervention(user)
+        self.interventions[0]["client"] = WRONG_CLIENT_ID
+        response = self._send_request(user)
+        assert response.status_code == 400
+        assert response.status_text == "Bad Request"
+        assert response.data["message"] == [{"client": ["Object with id={} does not exist.".format(WRONG_CLIENT_ID)]}]
 
     def test_authenticated_request_with_null_client_id_supplied_returns_400(self):
         user = User.objects.create(username="adventure", password="No1Knows!t")
@@ -201,7 +196,6 @@ class InterventionAPITestCase(TestCase):
         assert response.data["message"] == [{"client": ["This field may not be null."]}]
 
     def test_authenticated_user_with_duplicate_intervention_submission_returns_200(self):
-        
         user = User.objects.create(username="adventure", password="No1Knows!t")
         self._setup_intervention(user)
         self.interventions[0]["hts_result"] = None
@@ -215,7 +209,6 @@ class InterventionAPITestCase(TestCase):
         assert new_response.data["message"] == "Duplicate Record! Record not created since its a duplicate."
 
     def _setup_intervention(self, user):
-
         client = Client.objects.create(
             first_name="Lady",
             last_name="Bird",
@@ -240,7 +233,6 @@ class InterventionAPITestCase(TestCase):
         self.interventions[0]["implementing_partner"] = implementing_partner.code
 
     def _send_request(self, user):
-        
         factory = APIRequestFactory()
         request = factory.post(
             "api/v1/interventions", self.interventions, format="json"
