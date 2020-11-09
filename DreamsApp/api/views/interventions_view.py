6ce106dfcc749cd372f1a0e9c7a29407d3ec5c54
aonomike <aonomike@gmail.com>
2020-11-09 16:23:20 +0300

    Implement custom dreams responses
    - Implement Validation errors

--- DreamsApp/api/views/interventions_view.py
+++ DreamsApp/api/views/interventions_view.py
@@ -8,10 +8,10 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 from DreamsApp.api.serializers import InterventionSerializer
-from DreamsApp.api.status_codes_mixin import StatusCodesMixin
+from DreamsApp.api.response_status_mixin import ResponseStatusMixin
 
 
-class InterventionCreateView(CreateAPIView):
+class InterventionCreateView(CreateAPIView, ResponseStatusMixin):
     serializer_class = InterventionSerializer
     renderer_classes = [JSONRenderer]
     authentication_classes = [SessionAuthentication, BasicAuthentication]
@@ -24,35 +24,19 @@ class InterventionCreateView(CreateAPIView):
         try:
             self.perform_create(serializer)
         except (UnsupportedMediaType, ParseError) as e:
-            response_body = {'status': StatusCodesMixin.ERROR_SERIALIZATION_ERROR}
+            response_body = {'status': self.ERROR_SERIALIZATION_ERROR}
             return Response(status=200, data=response_body)
+
         except ValidationError as e:
-
-            response_body = {'status': StatusCodesMixin.ERROR_VALIDATION_ERROR, "errors": []}
-            for field in dict(e.detail).keys():
-                if field == 'hts_result':
-                    response_body["errors"].append({field: StatusCodesMixin.ERROR_VALIDATION_HTS_RESULT_NOT_FOUND})
-                elif field == 'pregnancy_test_result':
-                    response_body["errors"].append(
-                        {field: StatusCodesMixin.ERROR_VALIDATION_PREGNANCY_TEST_RESULT_NOT_FOUND})
-                elif field == 'intervention_type':
-                    response_body["errors"].append(
-                        {field: StatusCodesMixin.ERROR_VALIDATION_INTERVENTION_TYPE_NOT_FOUND})
-                elif field == 'implementing_partner':
-                    response_body["errors"].append({field: StatusCodesMixin.ERROR_VALIDATION_IP_NOT_FOUND})
-                elif field == 'external_organisation':
-                    response_body["errors"].append(
-                        {field: StatusCodesMixin.ERROR_VALIDATION_EXTERNAL_ORGANISATION_NOT_FOUND})
-                elif field == 'client':
-                    response_body["errors"].append({field: StatusCodesMixin.ERROR_VALIDATION_CLIENT_NOT_FOUND})
-                elif field == 'created_by':
-                    response_body["errors"].append({field: StatusCodesMixin.ERROR_VALIDATION_USER_NOT_FOUND})
-                else:
-                    response_body["errors"].append({field: StatusCodesMixin.ERROR_VALIDATION_ERROR})
-
+            errors = self.extract_response_errors(e.get_codes())
+            response_body = {'status': self.ERROR_VALIDATION_ERROR, "errors": errors}
             return Response(status=200, data=response_body)
 
-        response_body = {'status': StatusCodesMixin.SUCCESS_CREATED}
+        except DataError as e:
+            response_body = {'status': self.SUCCESS_DUPLICATE_IGNORED, 'message': 'Success'}
+            return Response(status=status.HTTP_200_OK, data=response_body)
+
+        response_body = {'status': self.SUCCESS_CREATED}
         return Response(status=status.HTTP_201_CREATED, data=response_body)
 
     def perform_create(self, serializer):
--- DreamsApp/tests/api/test_intervention_api.py
+++ DreamsApp/tests/api/test_intervention_api.py
@@ -1,4 +1,6 @@
-from DreamsApp.api.status_codes_mixin import StatusCodesMixin
+from rest_framework import status
+
+from DreamsApp.api.response_status_mixin import ResponseStatusMixin
 from DreamsApp.models import Intervention
 from DreamsApp.tests.api.api_test_case import APITestCase
 
@@ -17,10 +19,10 @@ class InterventionAPITestCase(APITestCase):
         response = self._send_request(test_data['user'], test_data['request_body'])
 
         self.assertEquals(response.status_code, 200, "Expected response status code of 200")
-        self.assertEquals(response.data["status"], StatusCodesMixin.ERROR_VALIDATION_ERROR,
+        self.assertEquals(response.data["status"], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
                           'Expected ERROR_VALIDATION_ERROR status code.')
 
-        created_by_field_error = {'created_by': StatusCodesMixin.ERROR_VALIDATION_USER_NOT_FOUND}
+        created_by_field_error = {'created_by': ResponseStatusMixin.ERROR_VALIDATION_USER_NOT_FOUND}
         self.assertIn(created_by_field_error, response.data["errors"],
                       'Expected created_by field amongst the returned error fields')
 
@@ -33,10 +35,10 @@ class InterventionAPITestCase(APITestCase):
         self.assertEquals(response.status_code, 200, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database after the api request")
-        self.assertEquals(response.data["status"], StatusCodesMixin.ERROR_VALIDATION_ERROR,
+        self.assertEquals(response.data["status"], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
                           'Expected ERROR_VALIDATION_ERROR status code.')
 
-        client_field_error = {'created_by': StatusCodesMixin.ERROR_VALIDATION_CLIENT_NOT_FOUND}
+        client_field_error = {'client': ResponseStatusMixin.ERROR_VALIDATION_CLIENT_NOT_FOUND}
         self.assertIn(client_field_error, response.data["errors"],
                       'Expected client field amongst the returned error fields')
 
@@ -47,95 +49,103 @@ class InterventionAPITestCase(APITestCase):
 
         response = self._send_request(test_data['user'], test_data['request_body'])
 
-        self.assertEquals(response.status_code, 200, "Expected response status code of 200")
+        self.assertEquals(response.status_code, status.HTTP_200_OK, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database after the api request")
-        self.assertEquals(response.body.status, StatusCodesMixin.ERROR_SERIALIZATION_ERROR,
-                          'Expected ERROR_SERIALIZATION_ERROR status code.')
+        self.assertEquals(response.data['status'], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
+                          'Expected ERROR_VALIDATION_ERROR status code.')
 
-    def test_request_with_wrong_pregnancy_test_result_returns_bad_request(self):
+        hts_result_field_error = {'hts_result': ResponseStatusMixin.ERROR_VALIDATION_HTS_RESULT_NOT_FOUND}
+        self.assertIn(hts_result_field_error, response.data["errors"],
+                      'Expected client field amongst the returned error fields')
+
+    def test_request_with_wrong_pregnancy_test_result_returns_pregnancy_test_result_field_validation_error(self):
         wrong_pregnancy_test_result = 9999
         test_data = self._generate_test_data()
         test_data['request_body']['pregnancy_test_result'] = wrong_pregnancy_test_result
 
         response = self._send_request(test_data['user'], test_data['request_body'])
 
-        self.assertEquals(response.status_code, 200,
-                          "Expected response status code of 200 but instead received {}".format(response.status_code))
-        self.assertEquals(response.status_text, 'Bad Request',
-                          "Expected response status code of Bad Request but instead received {}".format(
-                              response.status_text))
+        self.assertEquals(response.status_code, status.HTTP_200_OK, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database after the api request")
-        self.assertEquals(response.body.status, StatusCodesMixin.ERROR_SERIALIZATION_ERROR,
-                          'Expected ERROR_SERIALIZATION_ERROR status code.')
+        self.assertEquals(response.data['status'], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
+                          'Expected ERROR_VALIDATION_ERROR status code.')
 
-    def test_request_with_wrong_intervention_type_returns_bad_request(self):
-        WRONG_INTERVENTION_TYPE = 9999
+        pregnancy_test_result_field_error = {
+            'pregnancy_test_result': ResponseStatusMixin.ERROR_VALIDATION_PREGNANCY_TEST_RESULT_NOT_FOUND}
+        self.assertIn(pregnancy_test_result_field_error, response.data["errors"],
+                      'Expected client field amongst the returned error fields')
+
+    def test_request_with_wrong_intervention_type_returns_intervention_type_field_validation_error(self):
+        wrong_intervention_type = 9999
         test_data = self._generate_test_data()
-        test_data['request_body']['intervention_type'] = WRONG_INTERVENTION_TYPE
+        test_data['request_body']['intervention_type'] = wrong_intervention_type
         response = self._send_request(test_data['user'], test_data['request_body'])
-        self.assertEquals(response.status_code, 200,
-                          "Expected response status code of 200 but instead received {}".format(response.status_code))
-        self.assertEquals(response.status_text, 'Bad Request',
-                          "Expected response status code of Bad Request but instead received {}".format(
-                              response.status_text))
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database after the api request")
-        self.assertEquals(response.body.status, StatusCodesMixin.ERROR_SERIALIZATION_ERROR,
-                          'Expected ERROR_SERIALIZATION_ERROR status code.')
+        self.assertEquals(response.data['status'], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
+                          'Expected ERROR_VALIDATION_ERROR status code.')
 
-    def test_request_with_wrong_implementing_partner_returns_bad_request(self):
-        WRONG_IMPLEMENTING_PARTNER = 9999
+        intervention_type_field_error = {
+            'intervention_type': ResponseStatusMixin.ERROR_VALIDATION_INTERVENTION_TYPE_NOT_FOUND}
+        self.assertIn(intervention_type_field_error, response.data["errors"],
+                      'Expected client field amongst the returned error fields')
+
+    def test_request_with_wrong_implementing_partner_returns_implementing_partner_field_validation_error(self):
+        wrong_implementing_partner = 9999
         test_data = self._generate_test_data()
-        test_data['request_body']['external_organisation'] = WRONG_IMPLEMENTING_PARTNER
+        test_data['request_body']['implementing_partner'] = wrong_implementing_partner
         response = self._send_request(test_data['user'], test_data['request_body'])
-        self.assertEquals(response.status_code, 200,
-                          "Expected response status code of 200 but instead received {}".format(response.status_code))
-        self.assertEquals(response.status_text, 'Bad Request',
-                          "Expected response status code of Bad Request but instead received {}".format(
-                              response.status_text))
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database after the api request")
-        self.assertEquals(response.body.status, StatusCodesMixin.ERROR_SERIALIZATION_ERROR,
-                          'Expected ERROR_SERIALIZATION_ERROR status code.')
+        self.assertEquals(response.data['status'], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
+                          'Expected ERROR_VALIDATION_ERROR status code.')
 
-    def test_request_with_wrong_external_organisation_returns_bad_request(self):
-        WRONG_EXTERNAL_ORGANISATION = 9999
+        implementing_partner_field_error = {
+            'implementing_partner': ResponseStatusMixin.ERROR_VALIDATION_IP_NOT_FOUND}
+        self.assertIn(implementing_partner_field_error, response.data["errors"],
+                      'Expected client field amongst the returned error fields')
+
+
+    def test_request_with_wrong_external_organisation_returns_external_organisation_field_validation_error(self):
+        wrong_external_organisation = 9999
         test_data = self._generate_test_data()
-        test_data['request_body']['external_organisation'] = WRONG_EXTERNAL_ORGANISATION
+        test_data['request_body']['external_organisation'] = wrong_external_organisation
         response = self._send_request(test_data['user'], test_data['request_body'])
-        self.assertEquals(response.status_code, 200,
-                          "Expected response status code of 200 but instead received {}".format(response.status_code))
-        self.assertEquals(response.status_text, 'Bad Request',
-                          "Expected response status code of Bad Request but instead received {}".format(
-                              response.status_text))
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database after the api request")
-        self.assertEquals(response.body.status, StatusCodesMixin.ERROR_SERIALIZATION_ERROR,
-                          'Expected ERROR_SERIALIZATION_ERROR status code.')
+        self.assertEquals(response.data['status'], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
+                          'Expected ERROR_VALIDATION_ERROR status code.')
 
-    def test_empty_request_body_returns_bad_request(self):
+
+        external_organisation_field_error = {
+            'external_organisation': ResponseStatusMixin.ERROR_VALIDATION_EXTERNAL_ORGANISATION_NOT_FOUND}
+        self.assertIn(external_organisation_field_error, response.data["errors"],
+                      'Expected client field amongst the returned error fields')
+
+    def test_empty_request_body_returns_validation_error(self):
         test_data = self._generate_test_data()
         response = self._send_request(test_data['user'], {})
-        self.assertEquals(response.status_code, 200,
-                          "Expected response status code of 200 but instead received {}".format(response.status_code))
-        self.assertEquals(response.status_text, 'Bad Request',
-                          "Expected response status code of Bad Request but instead received {}".format(
-                              response.status_text))
+        self.assertEquals(response.status_code, status.HTTP_200_OK, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database after the api request")
-        self.assertEquals(response.body.status, StatusCodesMixin.ERROR_SERIALIZATION_ERROR,
-                          'Expected ERROR_SERIALIZATION_ERROR status code.')
+        self.assertEquals(response.data['status'], ResponseStatusMixin.ERROR_VALIDATION_ERROR,
+                          'Expected ERROR_VALIDATION_ERROR status code.')
 
     def test_intervention_request_creates_a_record(self):
         test_data = self._generate_test_data()
         self.assertEquals(Intervention.objects.all().count(), 0,
                           "Expected no record from the database before the api request")
         response = self._send_request(test_data['user'], test_data['request_body'])
-        self.assertEquals(response.status_code, 201,
-                          "Expected response status code of 201")
-        self.assertEquals(response.data["status"], StatusCodesMixin.SUCCESS_CREATED,
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED, "Expected response status code of 201")
+        self.assertEquals(response.data["status"], ResponseStatusMixin.SUCCESS_CREATED,
                           'Expected SUCCESS_CREATED status code.')
         self.assertEquals(Intervention.objects.all().count(), 1,
                           "Expected one record from the database after the api request")
@@ -149,9 +159,9 @@ class InterventionAPITestCase(APITestCase):
                           "Expected one record from the database after the first api request")
 
         response = self._send_request(test_data['user'], test_data['request_body'])
-        self._send_request(test_data['user'], test_data['request_body'])
+        self.assertEquals(response.status_code, status.HTTP_200_OK, "Expected response status code of 200")
         self.assertEquals(Intervention.objects.all().count(), 1,
                           "Expected one record from the database after second api call")
 
-        self.assertEquals(response.data["status"], StatusCodesMixin.SUCCESS_CREATED,
+        self.assertEquals(response.data["status"], ResponseStatusMixin.SUCCESS_DUPLICATE_IGNORED,
                           'Expected SUCCESS_CREATED status code.')
