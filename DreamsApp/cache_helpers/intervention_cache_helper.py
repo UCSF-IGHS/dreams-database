7985ad50fcdebaa50ca42ea01c6d52f3f986969d
aonomike <aonomike@gmail.com>
2020-11-11 16:46:31 +0300

    Refactoring
    - Use more correct names

--- DreamsApp/cache_helpers/intervention_cache_helper.py
+++ DreamsApp/cache_helpers/intervention_cache_helper.py
@@ -5,9 +5,9 @@ from DreamsApp.models import InterventionType
 
 class InterventionCacheHelper:
     @classmethod
-    def delete_intervention_category_key_from_cache(self, client_id, intervention_type_id):
-        intervention_type = InterventionType.objects.get(code=intervention_type_id)
-        intervention_category = intervention_type.intervention_category.code
+    def delete_intervention_category_key_from_cache(self, client_id, intervention_type_code):
+        intervention_type = InterventionType.objects.get(code=intervention_type_code)
+        intervention_category_code = intervention_type.intervention_category.code
         intervention_type_category_cache_key = 'client-{}-intervention-type-category-{}'.format(client_id,
-                                                                                                intervention_category)
+                                                                                                intervention_category_code)
         cache.delete(intervention_type_category_cache_key)
