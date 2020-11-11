b6c8552d51baaf09ed8573b6c7baad6bad8d4e72
aonomike <aonomike@gmail.com>
2020-11-11 17:43:25 +0300

    Make helper method static

--- DreamsApp/cache_helpers/intervention_cache_helper.py
+++ DreamsApp/cache_helpers/intervention_cache_helper.py
@@ -4,8 +4,8 @@ from DreamsApp.models import InterventionType
 
 
 class InterventionCacheHelper:
-    @classmethod
-    def delete_intervention_category_key_from_cache(self, client_id, intervention_type_code):
+    @staticmethod
+    def delete_intervention_category_key_from_cache(client_id, intervention_type_code):
         intervention_type = InterventionType.objects.get(code=intervention_type_code)
         intervention_category_code = intervention_type.intervention_category.code
         intervention_type_category_cache_key = 'client-{}-intervention-type-category-{}'.format(client_id,
