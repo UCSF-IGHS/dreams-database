8e82fe05695cfe1e625013156cd2097885bcd111
aonomike <aonomike@gmail.com>
2020-11-11 21:31:03 +0300

    Add try catch on cache

--- DreamsApp/cache_helpers/intervention_cache_helper.py
+++ DreamsApp/cache_helpers/intervention_cache_helper.py
@@ -5,9 +5,12 @@ from DreamsApp.models import InterventionType
 
 class InterventionCacheHelper:
     @staticmethod
-    def delete_intervention_category_key_from_cache(client_id, intervention_type_code):
-        intervention_type = InterventionType.objects.get(code=intervention_type_code)
-        intervention_category_code = intervention_type.intervention_category.code
-        intervention_type_category_cache_key = 'client-{}-intervention-type-category-{}'.format(client_id,
+    def delete_intervention_category_key_from_cache(client_id: int, intervention_type_code: int):
+        try:
+            intervention_type = InterventionType.objects.get(code=intervention_type_code)
+            intervention_category_code = intervention_type.intervention_category.code
+            intervention_type_category_cache_key = 'client-{}-intervention-type-category-{}'.format(client_id,
                                                                                                 intervention_category_code)
-        cache.delete(intervention_type_category_cache_key)
+            cache.delete(intervention_type_category_cache_key)
+        except Exception as e:
+            return {'error': str(e)}
