e13b43014cc48ae4a9acf6bf397e2b6700fb59db
aonomike <aonomike@gmail.com>
2021-03-01 14:50:30 +0300

    Update with code from other files

--- DreamsApp/tests/validation_rules/service_delegation/test_service_delegation.py
+++ DreamsApp/tests/validation_rules/service_delegation/test_service_delegation.py
@@ -10,8 +10,21 @@ class TestDelegatedImplementingPartnerTestCase(XFTestCase):
     def tearDown(self):
         pass
 
+    def test_intervention_type_is_required(self):
+        self.assertFieldRequired(ServiceDelegation, 'intervention_type', 'intervention type is required')
+
     def test_main_implementing_partner_is_required(self):
         self.assertFieldRequired(ServiceDelegation, 'delegated_implementing_partner', 'delegated implementing partner is required')
 
     def test_delegated_implementing_partner_is_required(self):
         self.assertFieldRequired(ServiceDelegation, 'delegated_implementing_partner', 'delegated implementing partner is required')
+
+    def test_start_date_is_required(self):
+        self.assertFieldRequired(ServiceDelegation, 'start_date', 'delegation start_date is required')
+
+    def test_end_date_is_required(self):
+        self.assertFieldRequired(ServiceDelegation, 'end_date', 'delegation end_date is required')
+
+    def test_financial_year_is_required(self):
+        self.assertFieldRequired(ServiceDelegation, 'financial_year', 'financial year is required')
+    
