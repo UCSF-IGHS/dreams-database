1d46c08774d840fcc6346a31b52351b11b5312c2
aonomike <aonomike@gmail.com>
2021-03-30 15:58:28 +0300

    Fixes for PR comments

--- DreamsApp/business_rules/services/intervention_security_service.py
+++ DreamsApp/business_rules/services/intervention_security_service.py
@@ -24,3 +24,20 @@ class InterventionSecurityService:
             return checks_passed
         else:
             raise InterventionNotWithinUserRealmBusinessRuleException
+
+    @classmethod
+    def rule_try_can_add_intervention(cls, user, intervention):
+        checks_passed = []
+        vI002 = InterventionSecurityServiceChecks.check_client_belongs_to_ip(user, intervention.client)
+        if vI002 is None:
+            vI004 = InterventionSecurityServiceChecks.check_ip_has_active_delegation(
+                intervention.client.implementing_partner, user.implementing_partner)
+            if vI004 is not None:
+                checks_passed.append(vI004)
+        else:
+            checks_passed.append(vI002)
+
+        if checks_passed:
+            return checks_passed
+        else:
+            raise InterventionNotWithinUserRealmBusinessRuleException
