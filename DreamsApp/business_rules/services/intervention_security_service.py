5101f9b0426c6cc9225e0de748d655375833ccb5
aonomike <aonomike@gmail.com>
2021-03-30 10:36:58 +0300

    KEN 787
    - Tests and implementation for can add intervention

--- DreamsApp/business_rules/services/intervention_security_service.py
+++ DreamsApp/business_rules/services/intervention_security_service.py
@@ -1,6 +1,7 @@
 from DreamsApp.business_rules.check_rules.intervention_security_service_checks import InterventionSecurityServiceChecks
 from DreamsApp.exceptions import InterventionNotWithinUserRealmBusinessRuleException
 
+
 class InterventionSecurityService:
 
     @classmethod
@@ -21,4 +22,19 @@ class InterventionSecurityService:
                 checks_passed.append(vI003)
         else:
             checks_passed.append("VI002")
+        return checks_passed
+
+    @classmethod
+    def rule_try_can_add_intervention(self, user, intervention):
+        checks_passed = []
+        vI002 = InterventionSecurityServiceChecks.check_client_belongs_to_ip(user, intervention.client)
+        if vI002 is None:
+            vI004 = InterventionSecurityServiceChecks.check_ip_has_active_delegation(
+                intervention.client.implementing_partner, user.implementing_partner)
+            if vI004 is None:
+                raise InterventionNotWithinUserRealmBusinessRuleException
+            else:
+                checks_passed.append(vI004)
+        else:
+            checks_passed.append(vI002)
         return checks_passed
