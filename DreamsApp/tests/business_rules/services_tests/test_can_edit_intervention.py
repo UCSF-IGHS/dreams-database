fc0fc07dde1349dc911ac1d77c3dba7417b8b5c4
aonomike <aonomike@gmail.com>
2021-03-31 10:14:17 +0300

    Correct variable name

--- DreamsApp/tests/business_rules/services_tests/test_can_edit_intervention.py
+++ DreamsApp/tests/business_rules/services_tests/test_can_edit_intervention.py
@@ -7,10 +7,10 @@ from DreamsApp.tests.business_rules.services_tests.intervention_delegation_test_
 class RuleCanEditInterventionTestCase(InterventionDelegationTestCase):
 
     def test_when_intervention_belongs_to_user_ip(self):
-        intervention = self.test_data['intervention_by_ip_a_to_ip_a_client']
+        intervention_by_client_ip = self.test_data['intervention_by_ip_a_to_ip_a_client']
         client_ip_user = self.ip_a_user
         checks_passed = InterventionSecurityService.rule_try_can_edit_intervention(client_ip_user,
-                                                                                   intervention)
+                                                                                   intervention_by_client_ip)
         self.assertIn('VI003', checks_passed, 'Expected check VI003_intervention_belongs_to_user_ip to have passed')
 
     def test_when_intervention_does_not_belongs_to_user_ip(self):
