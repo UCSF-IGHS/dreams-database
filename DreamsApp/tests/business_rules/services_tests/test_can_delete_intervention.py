3bd60692eef88976b79aa463fb8e4d4ba5420440
aonomike <aonomike@gmail.com>
2021-04-01 05:16:44 +0300

    Test random use case

--- DreamsApp/tests/business_rules/services_tests/test_can_delete_intervention.py
+++ DreamsApp/tests/business_rules/services_tests/test_can_delete_intervention.py
@@ -6,17 +6,27 @@ from DreamsApp.tests.business_rules.services_tests.intervention_delegation_test_
 
 class RuleCanDeleteInterventionTestCase(InterventionDelegationTestCase):
 
-    def test_when_intervention_belongs_to_user_ip(self):
-        intervention_by_client_ip = self.test_data['intervention_by_ip_a_to_ip_a_client']
-        client_ip_user = self.ip_a_user
-        checks_passed = InterventionSecurityService.rule_try_can_delete_intervention(client_ip_user,
-                                                                                   intervention_by_client_ip)
-        self.assertIn('VI003', checks_passed, 'Expected check VI003_intervention_belongs_to_user_ip to have passed')
-
-    def test_when_intervention_does_not_belongs_to_user_ip(self):
+    def test_default_not_allowed(self):
         intervention_by_client_ip = self.test_data['intervention_by_ip_a_to_ip_a_client']
         another_ip_user = self.ip_b_user
 
         with self.assertRaises(InterventionNotWithinUserRealmBusinessRuleException):
             InterventionSecurityService.rule_try_can_delete_intervention(another_ip_user,
-                                                                                   intervention_by_client_ip)
+                                                                         intervention_by_client_ip)
+
+        with self.assertRaises(InterventionNotWithinUserRealmBusinessRuleException):
+            InterventionSecurityService.rule_try_can_delete_intervention(self.ip_c_user,
+                                                                         intervention_by_client_ip)
+
+        with self.assertRaises(InterventionNotWithinUserRealmBusinessRuleException):
+            intervention_by_second_random_ip = self.test_data['random_intervention']
+            random_ip_user = self.test_data['random_ip_user']
+            InterventionSecurityService.rule_try_can_delete_intervention(random_ip_user,
+                                                                         intervention_by_second_random_ip)
+
+    def test_when_intervention_belongs_to_user_ip(self):
+        intervention_by_client_ip = self.test_data['intervention_by_ip_a_to_ip_a_client']
+        client_ip_user = self.ip_a_user
+        checks_passed = InterventionSecurityService.rule_try_can_delete_intervention(client_ip_user,
+                                                                                     intervention_by_client_ip)
+        self.assertIn('VI003', checks_passed, 'Expected check VI003_intervention_belongs_to_user_ip to have passed')
