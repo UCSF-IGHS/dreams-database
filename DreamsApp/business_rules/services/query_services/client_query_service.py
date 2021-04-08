dcb2dd68b19fe3f721357037f6656316bd5c8a9b
aonomike <aonomike@gmail.com>
2021-04-08 09:03:06 +0300

    Refactor variable name

--- DreamsApp/business_rules/services/query_services/client_query_service.py
+++ DreamsApp/business_rules/services/query_services/client_query_service.py
@@ -1,7 +1,8 @@
 class ClientQueryService:
 
-    def __init__(self, ip_user):
-        self.ip_user = ip_user
+    def __init__(self, user):
+        self.user = user
 
     def get_clients(self, search_string=None, implementing_partner=None):
         pass
+    
