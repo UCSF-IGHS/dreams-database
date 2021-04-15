9bd61a006321b9de3e7737f696d7e546b8475218
aonomike <aonomike@gmail.com>
2021-04-15 19:05:11 +0300

    Implement Client query service
    - Fix test data generation

--- DreamsApp/business_rules/services/query_services/client_query_service.py
+++ DreamsApp/business_rules/services/query_services/client_query_service.py
@@ -1,11 +1,30 @@
+from django.db.models import Q
+
+from DreamsApp.models import Client, ServiceDelegation, Intervention
+
+
 class ClientQueryService:
 
     def __init__(self, user):
         self.user = user
 
     def get_clients(self):
-        clients = self.user
         if self.user:
-            clients = self.user.implementing_partner.select_related('client')
+            clients = Client.objects.select_related('implementing_partner', )
+            delegating_ips = self._get_delegating_ips(self.user.implementing_partner)
+            intervention_clients = self._get_intervention_clients(implementing_partner=self.user.implementing_partner)
+            clients = clients.filter(
+                Q(implementing_partner__in=delegating_ips) | Q(implementing_partner=self.user.implementing_partner) | Q(
+                    id__in=intervention_clients))
             return clients
 
+    def _get_delegating_ips(self):
+        delegations = ServiceDelegation.objects.filter(delegated_implementing_partner=self.user.implementing_partner)
+        delegating_ips = [delegation.main_implementing_partner for delegation in delegations]
+        return delegating_ips
+
+    def _get_intervention_clients(self):
+        intervention_queryset = Intervention.objects.select_related('client')
+        intervention_queryset = intervention_queryset.filter(implementing_partner=self.user.implementing_partner).filter(
+            ~Q(client__implementing_partner=self.user.implementing_partner)).only('client').distinct()
+        return [intervention.client.id for intervention in intervention_queryset]
