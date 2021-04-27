4936fe28b0eee88f75a542cf8e240a302d9e4165
aonomike <aonomike@gmail.com>
2021-04-27 12:03:24 +0300

    Add assertion on length of returned array

--- DreamsApp/tests/models/test_implementing_partner.py
+++ DreamsApp/tests/models/test_implementing_partner.py
@@ -7,11 +7,14 @@ class ImplementingPartnerTestCase(InterventionDelegationTestCase):
     def test_get_active_delegating_implementing_partners(self):
         test_data = self.create_test_data_for_ip_clients()
         active_delegation = self.create_delegation(delegating_implementing_partner=test_data['ip_x'],
-                               delegated_implementing_partner=test_data['ip_y'])
+                                                   delegated_implementing_partner=test_data['ip_y'])
         active_delegation_2 = self.create_delegation(delegating_implementing_partner=test_data['ip_x'],
-                                                   delegated_implementing_partner=test_data['ip_z'])
+                                                     delegated_implementing_partner=test_data['ip_z'])
         inactive_delegation = self.create_delegation(delegating_implementing_partner=test_data['ip_z'],
-                               delegated_implementing_partner=test_data['ip_y'], active=False)
+                                                     delegated_implementing_partner=test_data['ip_y'], active=False)
         delegated_ip = test_data['ip_y']
         active_delegating_ips = delegated_ip.get_active_delegating_implementing_partners
-        self.assertEquals(active_delegating_ips, [test_data['ip_x']])
+        self.assertEquals(active_delegating_ips, [test_data['ip_x']],
+                          'Expected the list of delegating IPs to have only ip_x')
+        self.assertEquals(len(active_delegating_ips), 1,
+                          'Expected the number of delegating IPs to be 1')
