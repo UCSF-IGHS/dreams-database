58e4fed70500f62f6b67127ec618b3d1efebc724
aonomike <aonomike@gmail.com>
2021-04-27 09:27:02 +0300

    Remove irrelevant line and correct for readability

--- DreamsApp/business_rules/services/query_services/intervention_query_service.py
+++ DreamsApp/business_rules/services/query_services/intervention_query_service.py
@@ -9,8 +9,7 @@ class InterventionQueryService:
         self.user = user
 
     def get_interventions(self):
-        if self.user:
-            test_i = Intervention.objects.filter(implementing_partner_id=4)
+        if self.user is not None :
             interventions = Intervention.objects.select_related('client', 'intervention_type')
             interventions = interventions.filter(~Q(voided=True), ~Q(client__voided=True))
             interventions = interventions.filter(Q(implementing_partner=self.user.implementing_partner) | Q(
