277887abb1c567d5512c533c8639eac2af4531c9
aonomike <aonomike@gmail.com>
2021-05-03 12:56:58 +0300

    Fix for PR comments

--- DreamsApp/tests/business_rules/services_tests/query_services_tests/test_get_intervention_query_service.py
+++ DreamsApp/tests/business_rules/services_tests/query_services_tests/test_get_intervention_query_service.py
@@ -8,35 +8,35 @@ class GetInterventionQueryServiceTestCase(InterventionDelegationTestCase):
 
     def test_default_not_allowed(self):
         test_data = self.create_test_data_for_ip_clients()
-        user = test_data['ip_y_user']
-        intervention = test_data['intervention_1_by_ip_z_to_ip_z_client_1']
-        query_service = InterventionQueryService(user=user)
+        ip_y_user = test_data['ip_y_user']
+        intervention = test_data['intervention_by_ip_z_to_ip_z_client_1']
+        query_service = InterventionQueryService(user=ip_y_user)
 
         with self.assertRaises(Intervention.DoesNotExist, msg='Expected not to find the intervention'):
             query_service.get_intervention(intervention.id)
 
     def test_when_intervention_is_voided(self):
         test_data = self.create_test_data_for_ip_clients()
-        user = test_data['ip_y_user']
-        intervention = test_data['voided_intervention_by_ip_y_to_ip_y_client_1']
-        query_service = InterventionQueryService(user=user)
+        ip_y_user = test_data['ip_y_user']
+        voided_intervention = test_data['voided_intervention_by_ip_y_to_ip_y_client_1']
+        query_service = InterventionQueryService(user=ip_y_user)
 
         with self.assertRaises(Intervention.DoesNotExist, msg='Expected not to find the intervention'):
-            query_service.get_intervention(intervention.id)
+            query_service.get_intervention(voided_intervention.id)
 
     def test_when_intervention_belongs_to_user_ip(self):
         test_data = self.create_test_data_for_ip_clients()
-        user = test_data['ip_z_user']
-        intervention_1_by_ip_z_to_ip_z_client_1 = test_data['intervention_1_by_ip_z_to_ip_z_client_1']
-        query_service = InterventionQueryService(user=user)
-        queried_intervention = query_service.get_intervention(intervention_1_by_ip_z_to_ip_z_client_1.id)
-        self.assertEquals(intervention_1_by_ip_z_to_ip_z_client_1, queried_intervention,
+        ip_z_user = test_data['ip_z_user']
+        intervention = test_data['intervention_by_ip_z_to_ip_z_client_1']
+        query_service = InterventionQueryService(user=ip_z_user)
+        queried_intervention = query_service.get_intervention(intervention.id)
+        self.assertEquals(intervention, queried_intervention,
                           'Expected queried intervention to equal intervention')
 
-        intervention_by_ip_z_to_ip_y_client_1 = test_data['intervention_by_ip_z_to_ip_y_client_1']
-        query_service = InterventionQueryService(user=user)
-        expected_intervention = query_service.get_intervention(intervention_by_ip_z_to_ip_y_client_1.id)
-        self.assertEquals(intervention_by_ip_z_to_ip_y_client_1, expected_intervention,
+        intervention = test_data['intervention_by_ip_z_to_ip_y_client_1']
+        query_service = InterventionQueryService(user=ip_z_user)
+        expected_intervention = query_service.get_intervention(intervention.id)
+        self.assertEquals(intervention, expected_intervention,
                           'Expected queried intervention to equal intervention')
 
     def test_when_user_ip_has_active_delegation_from_client_ip(self):
@@ -44,9 +44,9 @@ class GetInterventionQueryServiceTestCase(InterventionDelegationTestCase):
 
         self.create_delegation(delegating_implementing_partner=test_data['ip_y'],
                                delegated_implementing_partner=test_data['ip_x'])
-        intervention_1_by_ip_y_to_ip_y_client_1 = test_data['intervention_1_by_ip_y_to_ip_y_client_1']
-        user = test_data['ip_x_user']
-        query_service = InterventionQueryService(user=user)
-        expected_intervention = query_service.get_intervention(intervention_1_by_ip_y_to_ip_y_client_1.id)
-        self.assertEquals(intervention_1_by_ip_y_to_ip_y_client_1, expected_intervention,
+        intervention = test_data['intervention_1_by_ip_y_to_ip_y_client_1']
+        ip_x_user = test_data['ip_x_user']
+        query_service = InterventionQueryService(user=ip_x_user)
+        expected_intervention = query_service.get_intervention(intervention.id)
+        self.assertEquals(intervention, expected_intervention,
                           'Expected queried intervention to equal intervention')
