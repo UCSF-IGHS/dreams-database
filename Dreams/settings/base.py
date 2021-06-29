c43bc74076ff235fb374c5d5b87f388459ec500f
Vincent Yahuma <vincent.yahuma@ucglobalprograms.org>
2021-06-29 14:28:15 +0300

    change some files

--- Dreams/settings/base.py
+++ Dreams/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'su!2p!nf_9pw#$%zf$vk1-07$#c8n1m)@^be7*b7d5niuwa6%m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True   # Changed to False for custom error messaging!
